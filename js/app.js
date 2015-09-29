373988004eef98ba7c19a9fcc4b967549ccfffc4
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-09-29 20:52:29 +0200

    Add content + add fonts

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 * @Author: gicque_p
 * @Date:   2015-09-29 12:23:57
 * @Last Modified by:   gicque_p
-* @Last Modified time: 2015-09-29 12:33:44
+* @Last Modified time: 2015-09-29 19:51:36
 */
 
 $(document).ready(function() {
@@ -11,6 +11,14 @@ $(document).ready(function() {
 
     $('.header').animate({opacity: 1}, 1500, function() {
         $('.top').animate({opacity: 1}, {duration: 1500}, function() {});
-/*        $('.top').show("blind", "easeOutExpo", 1000);*/
+        // $('.top').show("blind", "easeOutExpo", 1000);
+    });
+
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 800);
+
+        return false;
     });
 });
