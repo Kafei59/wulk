db9312d2e5777b21423b85fcece715641d977afd
Kafei59 <pierrick.gicquelais@epitech.eu>
2016-02-22 15:31:06 +0100

    Update (need to fix menu)

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 * @Author: gicque_p
 * @Date:   2015-09-29 12:23:57
 * @Last Modified by:   gicque_p
-* @Last Modified time: 2015-10-07 12:44:08
+* @Last Modified time: 2015-11-20 16:14:14
 */
 
 $(document).ready(function() {
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
     $('a').click(function() {
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
+            scrollTop: $($(this).attr('href')).offset().top - 50
         }, 800);
 
         return false;
@@ -31,21 +31,23 @@ $(document).ready(function() {
         if (isVisible($('#me h3'))) {
             $('a[href=#me]').parent().addClass('disabled-link');
         } else {
-            $('a[href=#me]').parent().removeClass('disabled-link');            
+            $('a[href=#me]').parent().removeClass('disabled-link');
         }
         
         if (isVisible($('#projects h3'))) {
             $('a[href=#projects]').parent().addClass('disabled-link');
         } else {
-            $('a[href=#projects]').parent().removeClass('disabled-link');            
+            $('a[href=#projects]').parent().removeClass('disabled-link');
         }
 
         if (isVisible($('#contact h3'))) {
             $('a[href=#contact]').parent().addClass('disabled-link');
         } else {
-            $('a[href=#contact]').parent().removeClass('disabled-link');            
+            $('a[href=#contact]').parent().removeClass('disabled-link');
         }
     });
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
 
 function isVisible(elementToBeChecked) {
@@ -54,4 +56,23 @@ function isVisible(elementToBeChecked) {
     var TopElement = $(elementToBeChecked).offset().top;
     var BotElement = TopElement + $(elementToBeChecked).height();
     return ((BotElement <= BotView) && (TopElement >= TopView));
+}
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+
+    var mapOptions = {
+        center: new google.maps.LatLng(50.6403629, 3.0559652,21),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(50.6403629, 3.0559652,21),
+        title: "Je suis ici !"
+    });
+
+    marker.setMap(map);
 }
